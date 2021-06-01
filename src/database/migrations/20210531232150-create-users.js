8a4c86eeb50d6c1e8fb904f0331e9dc01d4ecfc1
deko possas <andrepossaster@gmail.com>
2021-05-31 21:24:32 -0300

    corrigi o nome da tabela

--- src/database/migrations/20210531232150-create-users.js
+++ src/database/migrations/20210531232150-create-users.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    return queryInterface.createTable('user', {
+    return queryInterface.createTable('users', {
       id: {
         type: Sequelize.INTEGER,
         primaryKey: true,
