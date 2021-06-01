bf09b86f85db2177a0a13fa320c3383314d3c501
deko possas <andrepossaster@gmail.com>
2021-05-31 21:15:25 -0300

    config sequelize

--- src/app/models/index.js
+++ src/app/models/index.js
@@ -7,12 +7,9 @@ const basename = path.basename(__filename);
 const config = require('../../config/database');
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
+
 
 fs
   .readdirSync(__dirname)
