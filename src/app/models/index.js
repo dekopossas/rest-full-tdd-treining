6bdaf253cb55e294c14219fa901de4bac59fd55a
deko possas <andrepossaster@gmail.com>
2021-05-31 21:12:39 -0300

    nao temos config env mais

--- src/app/models/index.js
+++ src/app/models/index.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require(__dirname + '/../config/config.json');
 const db = {};
 
 let sequelize;
