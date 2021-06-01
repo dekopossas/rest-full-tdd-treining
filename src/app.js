61a3325e4ac0b42a543c467be2512a0d08954faa
deko possas <andrepossaster@gmail.com>
2021-05-31 21:23:41 -0300

    corrigido function express

--- src/app.js
+++ src/app.js
@@ -2,14 +2,16 @@ const express = require('express');
 
 class AppController {
   constructor() {
-    this.express = express;
+    this.express = express();
 
     this.middlewares();
     this.router();
   }
+
   middlewares() {
     this.express.use(express.json());
   }
+
   router() {
     this.express.use(require('./routes'));
   }
