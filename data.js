00363550bc8aac3352f4e8ec574e70ab39618ec3
deko possas <andrepossaster@gmail.com>
2021-06-01 21:24:50 -0300

    retornando um array com todos os employes

--- data.js
+++ data.js
@@ -3,10 +3,22 @@ let rl = linebyline('./banco.txt');
 
 rl.on('line', (line, lineCount) => {
   // faça algo com a linha
+  let emploies = [];
+  if(!line) return;
   if (lineCount % 2 === 1){
-    
-    console.log(`Estou na linha: ${lineCount} com o valor ${line}`)
+    let dados = line.split(';');
+    const employ = {
+      DataCad: dados[0],
+      Cargo: dados[1],
+      Cpf: dados[2],
+      Nome: dados[3],
+      UfNasc: dados[4],
+      Salario: dados[5],
+      Status: dados[6],
+    }
+    emploies.push(employ)
   }
+  return emploies;
 });
 
 rl.on('error', (e) => {
