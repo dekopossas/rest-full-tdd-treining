31f5917351f8f35702c6cc1d344c028529d41969
deko possas <andrepossaster@gmail.com>
2021-08-25 11:08:29 -0300

    digita linha por linha

---
+++ linerByLine.js
@@ -0,0 +1,13 @@
+const linebyline = require('linebyline');
+let rl = linebyline('./banco.txt');
+rl.on('line', (line, lineCount) => {
+  // faça algo com a linha
+  if (lineCount % 2 === 1){
+    
+    console.log(`Estou na linha: ${lineCount} com o valor ${line}`)
+  }
+});
+rl.on('error', (e) => {
+  console.error(e)
+  // deu erro!
+});
