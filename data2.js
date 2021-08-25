7d73a74ae7cc826f1b661bf6c1da60d4d4d6defb
deko possas <andrepossaster@gmail.com>
2021-08-25 10:58:44 -0300

    digita objetos separados por virgula

---
+++ data2.js
@@ -0,0 +1,24 @@
+const linebyline = require('linebyline');
+let rl = linebyline('./banco.txt');
+rl.on('line', (line, lineCount) => {
+  // faça algo com a linha
+  let emploies = [];
+  if(!line) return;
+  if (lineCount % 2 === 1){
+    let dados = line.split(';');
+    const employ = {
+      DataCad: dados[0],
+      Cargo: dados[1],
+      Cpf: dados[2],
+      Nome: dados[3],
+      UfNasc: dados[4],
+      Salario: dados[5],
+      Status: dados[6],
+    }
+    console.log(employ,',')
+  }
+});
+rl.on('error', (e) => {
+  console.error(e)
+  // deu erro!
+});
